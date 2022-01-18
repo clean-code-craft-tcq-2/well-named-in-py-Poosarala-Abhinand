b2c16ec3974f7cba5800076f45d6f6bc709876bd
Abhinand <90901127+Poosarala-Abhinand@users.noreply.github.com>
2022-01-18 21:38:14 +0530

    Adding exception handler

--- color_code_handler.py
+++ color_code_handler.py
@@ -4,11 +4,9 @@ from constants import MAJOR_COLORS, MINOR_COLORS
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
   major_index = zero_based_pair_number // len(MINOR_COLORS)
-  if major_index >= len(MAJOR_COLORS):
-    raise Exception('Major index out of range')
+  exception_handler.check_for_exception(major_index,MAJOR_COLORS)
   minor_index = zero_based_pair_number % len(MINOR_COLORS)
-  if minor_index >= len(MINOR_COLORS):
-    raise Exception('Minor index out of range')
+  exception_handler.check_for_exception(minor_index,MINOR_COLORS)  
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
 
 def get_pair_number_from_color(major_color, minor_color):
