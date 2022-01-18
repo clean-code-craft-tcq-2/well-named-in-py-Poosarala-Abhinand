fbdbeea7a5207bbe4734cd531c1922bb3c96b91d
Abhinand <90901127+Poosarala-Abhinand@users.noreply.github.com>
2022-01-18 21:40:04 +0530

    Update exception_handler.py

--- exception_handler.py
+++ exception_handler.py
@@ -1,3 +1,5 @@
-def check_for_exception(index, constant):
+# This file contains methods for checking exceptions
+
+def check_for_index_exception(index, constant):
   if index >= len(constant):
     raise Exception('Index out of range: ',index)
