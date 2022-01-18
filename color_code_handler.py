9ada08f3571c9bf6883829c6d1ca8cd797415822
Abhinand <90901127+Poosarala-Abhinand@users.noreply.github.com>
2022-01-18 21:40:25 +0530

    Update color_code_handler.py

--- color_code_handler.py
+++ color_code_handler.py
@@ -4,9 +4,9 @@ from constants import MAJOR_COLORS, MINOR_COLORS
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
   major_index = zero_based_pair_number // len(MINOR_COLORS)
-  exception_handler.check_for_exception(major_index,MAJOR_COLORS)
+  exception_handler.check_for_index_exception(major_index,MAJOR_COLORS)
   minor_index = zero_based_pair_number % len(MINOR_COLORS)
-  exception_handler.check_for_exception(minor_index,MINOR_COLORS)  
+  exception_handler.check_for_index_exception(minor_index,MINOR_COLORS)  
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
 
 def get_pair_number_from_color(major_color, minor_color):
